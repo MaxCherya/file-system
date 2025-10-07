ec0aab240da82440c57eb055f06bad5ee91bed65
Max <maxchergik@gmail.com>
2025-10-07 12:26:21 +0300

    added configs for frontend deployment

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -9,8 +9,9 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
   {
     ignores: [
       "node_modules/**",
@@ -20,6 +21,19 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
-];
 
-export default eslintConfig;
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+
+      "react-hooks/exhaustive-deps": "warn",
+
+      "@next/next/no-img-element": "off",
+    },
+  },
+];
